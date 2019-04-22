7d24c29aa81012a353c1ea9a1be9943b6c59d623
Gaurav Gupta <gaurav.gupta@socgen.com>
2019-04-22 19:16:52 +0000

    Removed Duplicate Print

--- voiceapp.cs
+++ voiceapp.cs
@@ -9,10 +9,6 @@ namespace HelloWorld
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
-            Console.WriteLine("Press any key to exit.");
-
         }
     }
 }
